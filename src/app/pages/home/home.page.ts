8b69ce671e53610c302aca9902592d5818e09bae
Ragav-KS <siva.s.ragav@gmail.com>
2023-02-19 01:57:51 +0530

    remove redundant testing button

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,13 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
-import { firstValueFrom } from 'rxjs';
-import { Transaction } from 'src/app/entities/transaction';
 import { GmailService } from 'src/app/services/Gmail/gmail.service';
 import { JobsService } from 'src/app/services/Jobs/jobs.service';
-import { ContentProcessorService } from 'src/app/services/Processors/Content/content-processor.service';
-import { MailProcessorService } from 'src/app/services/Processors/Mail/mail-processor.service';
 import { SqliteStorageService } from 'src/app/services/Storage/SQLite/sqlite-storage.service';
-import { Repository } from 'typeorm';
 
 @Component({
   selector: 'app-home',
@@ -15,8 +10,6 @@ import { Repository } from 'typeorm';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage implements OnInit {
-  loggedIn = false;
-
   constructor(
     private gmailSrv: GmailService,
     private sqliteSrv: SqliteStorageService,
@@ -24,25 +17,12 @@ export class HomePage implements OnInit {
     private navCtrl: NavController
   ) {}
 
-  private transactionsRepo!: Repository<Transaction>;
+  loggedIn = false;
 
   ngOnInit(): void {
     this.gmailSrv.loggedIn.subscribe((value) => {
       this.loggedIn = value;
     });
-
-    this.loadRepo();
-  }
-
-  async loadRepo() {
-    if (!this.sqliteSrv.DBReady) {
-      await firstValueFrom(this.sqliteSrv.DBReadyEmitter);
-    }
-
-    this.transactionsRepo = this.sqliteSrv.AppDataSource.getRepository(
-      'Transaction'
-    ) as Repository<Transaction>;
-    console.info('>>>> [sqlite] Repository Loaded');
   }
 
   async handleLogin() {
@@ -59,11 +39,6 @@ export class HomePage implements OnInit {
         this.sqliteSrv.saveDB();
       },
     });
-  }
-
-  async handleDBRead() {
-    const savedPhotos = await this.transactionsRepo.find();
-    console.log(savedPhotos);
   }
 
   handleGoToTransactions() {
