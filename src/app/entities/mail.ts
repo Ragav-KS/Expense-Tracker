5598123874db88bd88470cd37c9df0adcee922b3
Ragav-KS <siva.s.ragav@gmail.com>
2023-03-14 00:03:54 +0530

    introduce mail entity

---
+++ src/app/entities/mail.ts
@@ -0,0 +1,18 @@
+import { Entity, JoinColumn, OneToOne, PrimaryColumn } from 'typeorm';
+import { Transaction } from './transaction';
+
+@Entity({
+  name: 'Mails',
+})
+export class Mail {
+  @PrimaryColumn()
+  id!: string;
+
+  @OneToOne(() => Transaction, (transaction) => transaction.id, {
+    cascade: ['insert', 'update'],
+    nullable: true,
+    eager: true,
+  })
+  @JoinColumn({ name: 'transaction' })
+  transaction!: Transaction;
+}
